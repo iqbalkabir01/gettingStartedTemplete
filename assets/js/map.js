f113dc30e67f649e4e2495d6ccc47358de5ebf55
iqbalkabir01 <Iqbal@Imrans-MacBook-Pro.local>
2016-05-17 21:05:47 -0600

    more fixes

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,13 +1,19 @@
 //
-function initMap()
+$(document).ready(function()
 {
-  var mapDiv = docoument.getElementByID('map');
-  var map = new google.maps.Map(mapDiv,{
-    center:{Lat: 51.0486, lng: -114.0708},
-    zoom: 10, draggble:false,
-    zoomcontrol: false,
-    scrollewheel: false,
-      disableDoubleClickZoom: true,
-      mapTypecontrol: false
-  });
-}
+  var marker;
+  var geocoder = new google.maps.geocoder();
+  var myLatlng= new google.maps.latlng(51.0486, -114.0708);
+
+  var mapOptions =
+  {
+    center: myLatlng,
+    zoom: 10,
+    draggable:false,
+    zooomControl:false,
+    scaleControl: false,
+    disableDoubleClickZoom: true,
+    mapTypeControl: false
+  }
+  var map = new google.maps.Map($("#map").get(0), mapOptions);
+});
