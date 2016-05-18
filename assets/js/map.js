9af6814a96cb85247fa16b8908dbe348f1477af6
iqbalkabir01 <Iqbal@Imrans-MacBook-Pro.local>
2016-05-17 20:27:41 -0600

    map fix

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,9 +1,12 @@
 //
 function initMap()
 {
-  var mapDiv docoument.getElementById('map');
-  var map=new google.maps.Map(mapDiv,{
-    center:{Lat:51.0486, lng: -114.0708},
-    zoom:10
+  var mapDiv = docoument.getElementById('map');
+  var map= new google.maps.Map(mapDiv,{
+    center:{Lat: 51.0486, lng: -114.0708},
+    zoom: 10, draggble:false,
+    zoomcontrol: false,
+    scrollewheel: false,
+      disableDoubleClickZoom: true
   });
 }
