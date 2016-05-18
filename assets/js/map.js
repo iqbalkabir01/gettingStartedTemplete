c7ddc4479cd1aec9914e52e10cfb56d08067fdd3
iqbalkabir01 <Iqbal@Imrans-MacBook-Pro.local>
2016-05-17 20:31:53 -0600

    fix for getElementByID

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,12 +1,13 @@
 //
 function initMap()
 {
-  var mapDiv = docoument.getElementById('map');
-  var map= new google.maps.Map(mapDiv,{
+  var mapDiv = docoument.getElementByID('map');
+  var map = new google.maps.Map(mapDiv,{
     center:{Lat: 51.0486, lng: -114.0708},
     zoom: 10, draggble:false,
     zoomcontrol: false,
     scrollewheel: false,
-      disableDoubleClickZoom: true
+      disableDoubleClickZoom: true,
+      mapTypecontrol: false
   });
 }
